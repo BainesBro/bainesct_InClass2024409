9a463469cca60759ce710ffe73fc8e8f190d1a92
bainesct <bainesct@VDI-LCB-IC-025>
2024-04-11 11:46:38 -0400

    Add project files.

---
+++ bainesct_InClass2024409/default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace bainesct_InClass2024409
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+            
+        {
+            lbFruits.Items.Add("Strawberry");
+
+        }
+
+        protected void lbFruits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            lblChosenFruit.Text = lbFruits.SelectedValue;
+        }
+        // I need an event that will run when the user,
+        // in their browser, selects a fruit from the list box
+        // "event-driven" programming.
+        // Our event is The user selected an item from the list box
+    }
+}
